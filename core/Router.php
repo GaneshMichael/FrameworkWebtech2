63212078a3bb43d06ba0842836bca633b36b7265
Michael Ganesh <m.a.s.ganesh@st.hanze.nl>
2023-03-30 22:10:38 +0200

    added request

--- core/Router.php
+++ core/Router.php
@@ -4,13 +4,25 @@ namespace app\core;
 
 class Router {
     protected array $routes = [];
+    public Request $request;
+
+    public function __construct(\app\core\Request $request)
+    {
+        $this->request = $request;
+    }
     public function get($path, $callback)
     {
-        $this ->routes[$path] = $callback;
+        $this->routes[$path] = $callback;
     }
 
     public function resolve()
     {
-
+     $path = $this->request->getPath();
+     $method = $this->request->getMethod();
+     $callback = $this->routes[$method][$path];
+     echo '<pre>';
+     var_dump($callback);
+     echo '</pre>';
+     exit;
     }
 }

13b253956d07b35f3510e5a4e3d4c090b4483918
Michael Ganesh <m.a.s.ganesh@st.hanze.nl>
2023-05-28 22:02:16 +0200

    Update home.php

--- views/home.php
+++ views/home.php
@@ -1,9 +1,15 @@
-<html>
+<!DOCTYPE html>
+<html lang="en">
 <head>
+    <meta charset="UTF-8">
     <title>Home</title>
+    {{css}}
 </head>
 <body>
-    <h1>Home</h1>
+    {{header}}
+    <div class="container">
+        <h1>Home</h1>
+    </div>
+    {{footer}}
 </body>
-
 </html>

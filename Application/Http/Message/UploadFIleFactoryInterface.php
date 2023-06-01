2775d71ac60d03ec8157f26ff56c2b0194a3917c
Michael Ganesh <m.a.s.ganesh@st.hanze.nl>
2023-06-01 11:15:46 +0200

    Update UploadFIleFactoryInterface.php

--- Application/Http/Message/UploadFIleFactoryInterface.php
+++ Application/Http/Message/UploadFIleFactoryInterface.php
@@ -2,31 +2,14 @@
 
 namespace app\Application\Http\Message;
 
-interface UploadedFileFactoryInterface
+interface UploadFIleFactoryInterface
 {
     /**
-     * Create a new uploaded file.
+     * Create a new URI.
      *
-     * If a size is not provided it will be determined by checking the size of
-     * the stream.
+     * @param string $uri The URI to parse.
      *
-     * @link http://php.net/manual/features.file-upload.post-method.php
-     * @link http://php.net/manual/features.file-upload.errors.php
-     *
-     * @param StreamInterface $stream The underlying stream representing the
-     *     uploaded file content.
-     * @param int $size The size of the file in bytes.
-     * @param int $error The PHP file upload error.
-     * @param string $clientFilename The filename as provided by the client, if any.
-     * @param string $clientMediaType The media type as provided by the client, if any.
-     *
-     * @throws \InvalidArgumentException If the file resource is not readable.
+     * @throws \InvalidArgumentException If the given URI cannot be parsed.
      */
-    public function createUploadedFile(
-        StreamInterface $stream,
-        int $size = null,
-        int $error = \UPLOAD_ERR_OK,
-        string $clientFilename = null,
-        string $clientMediaType = null
-    ): UploadedFileInterface;
+    public function createUri(string $uri = '') : UriInterface;
 }

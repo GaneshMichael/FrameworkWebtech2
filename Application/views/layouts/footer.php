2b257bccf1acdfcc7d055b1acd5008107ed0b602
Michael Ganesh <m.a.s.ganesh@st.hanze.nl>
2023-06-06 22:25:23 +0200

    updaet

--- Application/views/layouts/footer.php
+++ Application/views/layouts/footer.php
@@ -2,7 +2,6 @@
     <div class="container-full">
         <div class="row">
             <div class="column-100 align-center">
-                <h3 class="text-white">© 2023 - WebTech 2 - framework</h3>
                 <p class="text-white">Created by Michael Ganesh</p>
             </div>
         </div>

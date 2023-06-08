c3475b2816d6808fff562e07634a21c6af98cd23
Michael Ganesh <m.a.s.ganesh@st.hanze.nl>
2023-06-08 13:21:42 +0200

    Update Router.php

--- Application/Routes/Router.php
+++ Application/Routes/Router.php
@@ -2,11 +2,9 @@
 
 namespace app\Application\Routes;
 
-use app\Application\Http\Message\ServerRequestInterface;
 
 class Router
 {
-    private const noRouteFound = 404;
-    private $routes;
     
+
 }

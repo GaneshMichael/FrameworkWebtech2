2940217084f6e11c77614a23ad9cf91230b2b7bd
Michael Ganesh <m.a.s.ganesh@st.hanze.nl>
2023-06-08 14:34:35 +0200

    clean

--- Application/Public/Index.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-require './vendor/autoload.php';
-
-use app\Application\Routes\Router;
-
--- Application/views/layouts/footer.php
+++
@@ -1,9 +0,0 @@
-<footer class="footer">
-    <div class="container-full">
-        <div class="row">
-            <div class="column-100 align-center">
-                <p class="text-white">Created by Michael Ganesh</p>
-            </div>
-        </div>
-    </div>
-</footer>

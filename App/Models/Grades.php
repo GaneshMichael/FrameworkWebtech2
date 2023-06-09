5091efcdcb3badbf97959e4a77516e10fe5d1d3b
Michael Ganesh <m.a.s.ganesh@st.hanze.nl>
2023-06-09 22:53:16 +0200

    grades model added and SQL query

---
+++ App/Models/Grades.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace app\App\Models;
+
+use app\App\core\Database;
+
+class Grades {
+    protected $db;
+    public $id = 0;
+    public $user_id = '';
+    public $exam_id = '';
+    public $grade = '';
+    public $created_at = '';
+    public $updated_at = '';
+    public $created_by = '';
+    public $updated_by = '';
+    public $student_id = '';
+
+    public function __construct() {
+        $this->db = new Database();
+    }
+
+    public function getAllGrades() {
+        $query = "SELECT * FROM Grades";
+        return $this->db->query($query);
+    }
+
+    public function createGrade() {
+        $query = "INSERT INTO Grades (user_id, exam_id, grade, created_at, updated_at, created_by, updated_by, student_id) 
+                  VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
+        $values = [
+            $this->user_id,
+            $this->exam_id,
+            $this->grade,
+            $this->created_at,
+            $this->updated_at,
+            $this->created_by,
+            $this->updated_by,
+            $this->student_id
+        ];
+        return $this->db->execute($query, $values);
+    }
+
+    public function getGradeById($id) {
+        $query = "SELECT * FROM Grades WHERE id = ?";
+        $values = [$id];
+        return $this->db->query($query, $values);
+    }
+
+    public function updateGrade() {
+        $query = "UPDATE Grades SET user_id = ?, exam_id = ?, grade = ?, 
+                  created_at = ?, updated_at = ?, created_by = ?, updated_by = ?, student_id = ? 
+                  WHERE id = ?";
+        $values = [
+            $this->user_id,
+            $this->exam_id,
+            $this->grade,
+            $this->created_at,
+            $this->updated_at,
+            $this->created_by,
+            $this->updated_by,
+            $this->student_id,
+            $this->id
+        ];
+        return $this->db->execute($query, $values);
+    }
+
+    public function deleteGrade($id) {
+        $query = "DELETE FROM Grades WHERE id = ?";
+        $values = [$id];
+        return $this->db->execute($query, $values);
+    }
+
+    // Andere methoden voor het ophalen, bijwerken en verwijderen van cijfers
+
+    public function __get($property) {
+        if (property_exists($this, $property)) {
+            return $this->$property;
+        }
+    }
+
+    public function __set($property, $value) {
+        if (property_exists($this, $property)) {
+            $this->$property = $value;
+        }
+    }
+}
--- App/core/Database/OsirisV2.sql
+++ App/core/Database/OsirisV2.sql
@@ -25,3 +25,14 @@ CREATE TABLE Exams (
                            teacher_ID INT NOT NULL
 );;
 
+DROP TABLE IF EXISTS Grades;
+CREATE TABLE Grades (
+                            ID INT PRIMARY KEY AUTO_INCREMENT,
+                            grade VARCHAR(255) NOT NULL,
+                            student_ID INT NOT NULL,
+                            exam_ID INT NOT NULL,
+                            created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+                            updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+                            FOREIGN KEY (student_ID) REFERENCES Student(id),
+                            FOREIGN KEY (exam_ID) REFERENCES Exams(id)
+);;

a381d9bf7f2bc728f1d85013718defa634d4dc7f
Michael Ganesh <m.a.s.ganesh@st.hanze.nl>
2023-06-09 14:42:47 +0200

    update

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'michael/framework-webtech2',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '4dacc129498f624f461412ff727e7c63053136e4',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'michael/framework-webtech2' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '4dacc129498f624f461412ff727e7c63053136e4',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
